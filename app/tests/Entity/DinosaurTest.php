3af24571bc75d9441788808da0390550adf0fdbb
Karlik <kkkj91@yahoo.pl>
2021-07-18 15:37:36 +0200

    chapter 1

---
+++ app/tests/Entity/DinosaurTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+    public function testThatYourComputerWorks(): void
+    {
+        $this->assertTrue(false);
+    }
+}
