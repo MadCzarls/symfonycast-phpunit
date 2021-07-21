133bf7ac2e6a0ca6f2f81f942548723f93a7e766
Karlik <kkkj91@yahoo.pl>
2021-07-21 12:43:26 +0200

    chapter 21

--- app/tests/Service/EnclosureBuilderIntegrationTest.php
+++ app/tests/Service/EnclosureBuilderIntegrationTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Service;
 
 use App\Entity\Dinosaur;
+use App\Entity\Enclosure;
 use App\Entity\Security;
 use App\Service\EnclosureBuilder;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,16 +14,26 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EnclosureBuilderIntegrationTest extends KernelTestCase
 {
-    public function testItBuildsEnclosureWithDefaultSpecifications(): void
+    protected function setUp(): void
     {
         self::bootKernel();
+        $this->truncateEntities(
+            [
+                Enclosure::class,
+                Security::class,
+                Dinosaur::class,
+            ]
+        );
+    }
+
+    public function testItBuildsEnclosureWithDefaultSpecifications(): void
+    {
         $container = static::getContainer();
         /** @var EnclosureBuilder $enclosureBuilder */
         $enclosureBuilder = $container->get(EnclosureBuilder::class);
         $enclosureBuilder->buildEnclosure();
 
-        /** @var EntityManagerInterface $entityManager */
-        $entityManager = $container->get(EntityManagerInterface::class);
+        $entityManager = $this->getEntityManager();
 
         /** @var EntityRepository $securityRepository */
         $securityRepository = $entityManager->getRepository(Security::class);
@@ -41,5 +52,38 @@ class EnclosureBuilderIntegrationTest extends KernelTestCase
             ->getSingleScalarResult();
 
         $this->assertSame(3, $dinosaurCount, 'Amount of dinosaurs is not the same');
+    }
+
+    /**
+     * @param string[] $entities
+     */
+    private function truncateEntities(array $entities): void
+    {
+        $connection = $this->getEntityManager()->getConnection();
+        $databasePlatform = $connection->getDatabasePlatform();
+        if ($databasePlatform->supportsForeignKeyConstraints()) {
+            $connection->executeQuery("SET session_replication_role = 'replica';"); //postgresql specific query
+        }
+
+        foreach ($entities as $entity) {
+            $query = $databasePlatform->getTruncateTableSQL(
+                $this->getEntityManager()->getClassMetadata($entity)->getTableName(),
+                true
+            );
+            $connection->executeStatement($query);
+        }
+
+        if (!$databasePlatform->supportsForeignKeyConstraints()) {
+            return;
+        }
+
+        $connection->executeQuery("SET session_replication_role = 'origin';"); //postgresql specific query
+    }
+
+    private function getEntityManager(): EntityManagerInterface
+    {
+        $container = static::getContainer();
+
+        return $container->get(EntityManagerInterface::class);
     }
 }
